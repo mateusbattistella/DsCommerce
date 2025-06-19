a562a86790472c71157f32f4e9ac8d0c1293a607
Mateus <battistella.mateus@gmail.com>
2025-06-19 11:00:05 -0300

    Posto new product e ResponseEntity

--- src/main/java/com/example/dscommerce/services/ProductService.java
+++ src/main/java/com/example/dscommerce/services/ProductService.java
@@ -1,9 +1,12 @@
 package com.example.dscommerce.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.hibernate.type.TrueFalseConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +29,26 @@ public class ProductService {
 		//return dto;
 		Product product = repository.findById(id).get();
 		return new ProductDTO(product);
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable) {		
+		Page<Product> result = repository.findAll(pageable);
+		return result.map(x -> new ProductDTO(x));
+	}
+	
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {		
+		Product entity = new Product();
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		
+		entity = repository.save(entity);
+		
+		//retorna o objeto salvo e atualizado
+		return new ProductDTO(entity);
 	}
 
 }
