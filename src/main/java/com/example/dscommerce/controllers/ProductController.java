34d1d7e0706160ba36bc4f72c6266c772e4d56be
Mateus <battistella.mateus@gmail.com>
2025-07-16 21:54:08 -0300

    Correção Tratamento exceção customizada.

--- src/main/java/com/example/dscommerce/controllers/ProductController.java
+++ src/main/java/com/example/dscommerce/controllers/ProductController.java
@@ -1,8 +1,6 @@
 package com.example.dscommerce.controllers;
 
 import java.net.URI;
-import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -18,10 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.example.dscommerce.dto.CustomError;
 import com.example.dscommerce.dto.ProductDTO;
-import com.example.dscommerce.entities.Product;
-import com.example.dscommerce.repositories.ProductRepository;
 import com.example.dscommerce.services.ProductService;
 
 @RestController
--- src/main/java/com/example/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/example/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -2,21 +2,21 @@ package com.example.dscommerce.controllers.handlers;
 
 import java.time.Instant;
 
-import org.hibernate.ResourceClosedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.example.dscommerce.dto.CustomError;
+import com.example.dscommerce.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-	@ExceptionHandler(ResourceClosedException.class)
-	public ResponseEntity<CustomError> resourceNotFound(ResourceClosedException e, HttpServletRequest request) {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
